a04e481f67a906fddbdb13382a15a98a943ebc14
warrenfl <43418128+warrenfl@users.noreply.github.com>
2018-10-03 19:10:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let keyAElement =document.getElementById("key-A")
+
+
+let audioAelement = document.getElementById("audio-A")
+
+keyAElement.onclick = () => {
+    audioAelement.play()
+}
+
+let keySelement =document.getElementById("key-S")
+
+let audioSelement = document.getElementById("audio-D")
+
+keySelement.onclick = () => {
+    audioSelement.play()
+}
+
+
